d5aa8937e5854afe2531e2556092f8b22c2e3a8d
Bill Wagner <bill.w.wagner@outlook.com>
2014-01-09 17:36:20 -0500

    Build the Addition problem logic.
    Note that HTML Element types are implicitly typed based on the string
    used for the element.
    Also note that jQuery templates are not installed.

--- MathPractice/MathPractice/app.js
+++ MathPractice/MathPractice/app.js
@@ -1,7 +1,42 @@
 /// <reference path="Scripts/typings/jquery/jquery.d.ts" />
 var AdditionProblem = (function () {
     function AdditionProblem(element) {
+        var _this = this;
+        var left = Math.floor(Math.random() * 25);
+        var right = Math.floor(Math.random() * 25);
+        var tmplate = "${left} + ${right} = ";
+
+        var renderedProblem = $.tmpl(tmplate, this);
+
+        this.correctAnswer = left + right;
+
+        var paragraph = document.createElement('p');
+        renderedProblem.appendTo(paragraph);
+
+        // not using JQuery here because the rendered problem
+        // is not in the DOM yet.
+        this.txt = document.createElement('input');
+        this.txt.type = 'text';
+        paragraph.appendChild(this.txt);
+
+        var button = document.createElement('input');
+        this.msg = document.createElement('span');
+        button.type = 'button';
+        button.value = "Check Answer";
+        button.onclick = function () {
+            return _this.checkAnswer();
+        };
+        paragraph.appendChild(button);
+        paragraph.appendChild(this.msg);
+
+        element.append(paragraph);
     }
+    AdditionProblem.prototype.checkAnswer = function () {
+        if (parseInt(this.txt.value) == this.correctAnswer)
+            this.msg.innerHTML = "You are correct";
+        else
+            this.msg.innerHTML = "Please try again";
+    };
     return AdditionProblem;
 })();
 
--- MathPractice/MathPractice/app.ts
+++ MathPractice/MathPractice/app.ts
@@ -2,8 +2,46 @@
 
 class AdditionProblem {
 
+    correctAnswer: number;
+    txt: HTMLInputElement;
+    msg: HTMLSpanElement;
+
     constructor(element: JQuery) {
+        var left = Math.floor(Math.random() * 25);
+        var right = Math.floor(Math.random() * 25);
+        var tmplate = "${left} + ${right} = ";
+
+        var renderedProblem = $.tmpl(tmplate, this);
+
+        this.correctAnswer = left + right;
+
+        var paragraph = document.createElement('p');
+        renderedProblem.appendTo(paragraph);
+
+        // not using JQuery here because the rendered problem 
+        // is not in the DOM yet.
+        this.txt = document.createElement('input');
+        this.txt.type = 'text';
+        paragraph.appendChild(this.txt);
+
+        var button = document.createElement('input');
+        this.msg = document.createElement('span');
+        button.type = 'button';
+        button.value = "Check Answer";
+        button.onclick = () => this.checkAnswer();
+        paragraph.appendChild(button);
+        paragraph.appendChild(this.msg);
+
+        element.append(paragraph);
     }
+
+    checkAnswer() {
+        if (parseInt(this.txt.value) == this.correctAnswer)
+            this.msg.innerHTML = "You are correct";
+        else
+            this.msg.innerHTML = "Please try again";
+    }
+
 
 }
 
