ce45158d9155a39dbf0eb7abf0a0946ec4803e26
Bill Wagner <bill.w.wagner@outlook.com>
2014-01-09 11:58:38 -0500

    Scope the controller in a Module
    This isn't an exported module, so legally, it can only be referenced
    locally.

--- Rolodex/Rolodex/MyScripts/contactsController.js
+++ Rolodex/Rolodex/MyScripts/contactsController.js
@@ -1,14 +1,17 @@
 ﻿/// reference path="../Scripts/typings/angularjs/angular.d.ts" />
 var contactsApp;
 
-contactsApp.controller('ContactsController', function ContactsController($scope, contactData) {
-    $scope.sortOrder = 'last';
-    $scope.hideMessage = "Hide Details";
-    $scope.showMessage = "Show Details";
-    $scope.contacts = contactData.getContacts();
-    $scope.toggleShowDetails = function (contact) {
-        contact.showDetails = !contact.showDetails;
-        return contact.showDetails;
-    };
-});
+var Rolodex;
+(function (Rolodex) {
+    contactsApp.controller('ContactsController', function ContactsController($scope, contactData) {
+        $scope.sortOrder = 'last';
+        $scope.hideMessage = "Hide Details";
+        $scope.showMessage = "Show Details";
+        $scope.contacts = contactData.getContacts();
+        $scope.toggleShowDetails = function (contact) {
+            contact.showDetails = !contact.showDetails;
+            return contact.showDetails;
+        };
+    });
+})(Rolodex || (Rolodex = {}));
 //# sourceMappingURL=contactsController.js.map
--- Rolodex/Rolodex/MyScripts/contactsController.ts
+++ Rolodex/Rolodex/MyScripts/contactsController.ts
@@ -3,37 +3,39 @@
 
 var contactsApp: ng.IModule;
 
-interface IContact {
-    first: string;
-    last: string;
-    address: string;
-    city: string;
-    state: string;
-    zipCode: number;
-    cellPhone: number;
-    homePhone: number;
-    workPhone: number;
-    showDetails: boolean
-}
+module Rolodex {
+
+    interface IContact {
+        first: string;
+        last: string;
+        address: string;
+        city: string;
+        state: string;
+        zipCode: number;
+        cellPhone: number;
+        homePhone: number;
+        workPhone: number;
+        showDetails: boolean
+    }
 
 
-interface IContactsScope extends ng.IScope {
-    sortOrder: string;
-    hideMessage: string;
-    showMessage: string;
-    contacts: IContact[];
-    toggleShowDetails: (contact: IContact) => boolean;
-}
+    interface IContactsScope extends ng.IScope {
+        sortOrder: string;
+        hideMessage: string;
+        showMessage: string;
+        contacts: IContact[];
+        toggleShowDetails: (contact: IContact) => boolean;
+    }
 
-contactsApp.controller('ContactsController',
-    function ContactsController($scope : IContactsScope, contactData) {
-        $scope.sortOrder = 'last';
-        $scope.hideMessage = "Hide Details";
-        $scope.showMessage = "Show Details";
-        $scope.contacts = contactData.getContacts();
-        $scope.toggleShowDetails = function (contact) {
-            contact.showDetails = !contact.showDetails;
-            return contact.showDetails;
-        }
+    contactsApp.controller('ContactsController',
+        function ContactsController($scope: IContactsScope, contactData) {
+            $scope.sortOrder = 'last';
+            $scope.hideMessage = "Hide Details";
+            $scope.showMessage = "Show Details";
+            $scope.contacts = contactData.getContacts();
+            $scope.toggleShowDetails = function (contact) {
+                contact.showDetails = !contact.showDetails;
+                return contact.showDetails;
+            }
     });
-
+}
