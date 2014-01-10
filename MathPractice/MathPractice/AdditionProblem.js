fcd982fe7c11af5063f8f56ad737edcb1e5054b3
Bill Wagner <bill.w.wagner@outlook.com>
2014-01-09 20:22:37 -0500

    Put AdditionProblem in a new file
    Starting to refactor into modules.

---
+++ MathPractice/MathPractice/AdditionProblem.js
@@ -0,0 +1,47 @@
+/// <reference path="Scripts/typings/jquery/jquery.d.ts" />
+
+var MathPractice;
+(function (MathPractice) {
+    var AdditionProblem = (function () {
+        function AdditionProblem(element) {
+            var _this = this;
+            this.left = Math.floor(Math.random() * 25);
+            this.right = Math.floor(Math.random() * 25);
+            var tmplate = "${left} + ${right} = ";
+
+            var renderedProblem = $.tmpl(tmplate, this);
+
+            this.correctAnswer = this.left + this.right;
+
+            var paragraph = document.createElement('p');
+            renderedProblem.appendTo(paragraph);
+
+            // not using JQuery here because the rendered problem
+            // is not in the DOM yet.
+            this.txt = document.createElement('input');
+            this.txt.type = 'text';
+            paragraph.appendChild(this.txt);
+
+            var button = document.createElement('input');
+            this.msg = document.createElement('span');
+            button.type = 'button';
+            button.value = "Check Answer";
+            button.onclick = function () {
+                return _this.checkAnswer();
+            };
+            paragraph.appendChild(button);
+            paragraph.appendChild(this.msg);
+
+            element.append(paragraph);
+        }
+        AdditionProblem.prototype.checkAnswer = function () {
+            if (parseInt(this.txt.value) == this.correctAnswer)
+                this.msg.innerHTML = "You are correct";
+            else
+                this.msg.innerHTML = "Please try again";
+        };
+        return AdditionProblem;
+    })();
+    MathPractice.AdditionProblem = AdditionProblem;
+})(MathPractice || (MathPractice = {}));
+//# sourceMappingURL=AdditionProblem.js.map
---
+++ MathPractice/MathPractice/AdditionProblem.ts
@@ -0,0 +1,56 @@
+/// <reference path="Scripts/typings/jquery/jquery.d.ts" />
+
+interface JQueryStatic {
+    tmpl(tmplate: string, data: any): JQuery;
+}
+
+module MathPractice {
+
+    export class AdditionProblem {
+
+        correctAnswer: number;
+        txt: HTMLInputElement;
+        msg: HTMLSpanElement;
+        left: number;
+        right: number;
+
+        constructor(element: JQuery) {
+            this.left = Math.floor(Math.random() * 25);
+            this.right = Math.floor(Math.random() * 25);
+            var tmplate = "${left} + ${right} = ";
+
+            var renderedProblem = $.tmpl(tmplate, this);
+
+            this.correctAnswer = this.left + this.right;
+
+            var paragraph = document.createElement('p');
+            renderedProblem.appendTo(paragraph);
+
+            // not using JQuery here because the rendered problem 
+            // is not in the DOM yet.
+            this.txt = document.createElement('input');
+            this.txt.type = 'text';
+            paragraph.appendChild(this.txt);
+
+            var button = document.createElement('input');
+            this.msg = document.createElement('span');
+            button.type = 'button';
+            button.value = "Check Answer";
+            button.onclick = () => this.checkAnswer();
+            paragraph.appendChild(button);
+            paragraph.appendChild(this.msg);
+
+            element.append(paragraph);
+        }
+
+        checkAnswer() {
+            if (parseInt(this.txt.value) == this.correctAnswer)
+                this.msg.innerHTML = "You are correct";
+            else
+                this.msg.innerHTML = "Please try again";
+        }
+
+
+    }
+}
+ 
