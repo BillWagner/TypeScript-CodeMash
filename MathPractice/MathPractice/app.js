bee1c3c6611a2d52b284022acc79c5bb3c13cf77
Bill Wagner <bill.w.wagner@outlook.com>
2014-01-09 20:19:36 -0500

    Put the AdditionProblem in a module
    Starting the refactoring journey

--- MathPractice/MathPractice/app.js
+++ MathPractice/MathPractice/app.js
@@ -1,50 +1,54 @@
 /// <reference path="Scripts/typings/jquery/jquery.d.ts" />
 
-var AdditionProblem = (function () {
-    function AdditionProblem(element) {
-        var _this = this;
-        this.left = Math.floor(Math.random() * 25);
-        this.right = Math.floor(Math.random() * 25);
-        var tmplate = "${left} + ${right} = ";
+var MathPractice;
+(function (MathPractice) {
+    var AdditionProblem = (function () {
+        function AdditionProblem(element) {
+            var _this = this;
+            this.left = Math.floor(Math.random() * 25);
+            this.right = Math.floor(Math.random() * 25);
+            var tmplate = "${left} + ${right} = ";
 
-        var renderedProblem = $.tmpl(tmplate, this);
+            var renderedProblem = $.tmpl(tmplate, this);
 
-        this.correctAnswer = this.left + this.right;
+            this.correctAnswer = this.left + this.right;
 
-        var paragraph = document.createElement('p');
-        renderedProblem.appendTo(paragraph);
+            var paragraph = document.createElement('p');
+            renderedProblem.appendTo(paragraph);
 
-        // not using JQuery here because the rendered problem
-        // is not in the DOM yet.
-        this.txt = document.createElement('input');
-        this.txt.type = 'text';
-        paragraph.appendChild(this.txt);
+            // not using JQuery here because the rendered problem
+            // is not in the DOM yet.
+            this.txt = document.createElement('input');
+            this.txt.type = 'text';
+            paragraph.appendChild(this.txt);
 
-        var button = document.createElement('input');
-        this.msg = document.createElement('span');
-        button.type = 'button';
-        button.value = "Check Answer";
-        button.onclick = function () {
-            return _this.checkAnswer();
+            var button = document.createElement('input');
+            this.msg = document.createElement('span');
+            button.type = 'button';
+            button.value = "Check Answer";
+            button.onclick = function () {
+                return _this.checkAnswer();
+            };
+            paragraph.appendChild(button);
+            paragraph.appendChild(this.msg);
+
+            element.append(paragraph);
+        }
+        AdditionProblem.prototype.checkAnswer = function () {
+            if (parseInt(this.txt.value) == this.correctAnswer)
+                this.msg.innerHTML = "You are correct";
+            else
+                this.msg.innerHTML = "Please try again";
         };
-        paragraph.appendChild(button);
-        paragraph.appendChild(this.msg);
-
-        element.append(paragraph);
-    }
-    AdditionProblem.prototype.checkAnswer = function () {
-        if (parseInt(this.txt.value) == this.correctAnswer)
-            this.msg.innerHTML = "You are correct";
-        else
-            this.msg.innerHTML = "Please try again";
-    };
-    return AdditionProblem;
-})();
+        return AdditionProblem;
+    })();
+    MathPractice.AdditionProblem = AdditionProblem;
+})(MathPractice || (MathPractice = {}));
 
 $(document).ready(function () {
     var el = $('#content');
     var button = $("<input></input>").val("Create New Problem").attr("type", "button").bind("click", function () {
-        var problem = new AdditionProblem(el);
+        var problem = new MathPractice.AdditionProblem(el);
     });
     el.append(button);
 });
