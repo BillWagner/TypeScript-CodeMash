8147aa6f243a5432d88720c18dd2766dd9a79b69
Bill Wagner <bill.w.wagner@outlook.com>
2014-01-09 20:52:13 -0500

    Move common code to the base class
    Notice that base class fields are protected by default.

--- MathPractice/MathPractice/AdditionProblem.js
+++ MathPractice/MathPractice/AdditionProblem.js
@@ -18,40 +18,8 @@ var MathPractice;
 
             this.correctAnswer = this.left + this.right;
 
-            this.buildHtml(element, tmplate);
+            _super.prototype.buildHtml.call(this, element, tmplate);
         }
-        AdditionProblem.prototype.buildHtml = function (element, tmplate) {
-            var _this = this;
-            var renderedProblem = $.tmpl(tmplate, this);
-            var paragraph = document.createElement('p');
-
-            renderedProblem.appendTo(paragraph);
-
-            // not using JQuery here because the rendered problem
-            // is not in the DOM yet.
-            this.txt = document.createElement('input');
-            this.txt.type = 'text';
-            paragraph.appendChild(this.txt);
-
-            var button = document.createElement('input');
-            this.msg = document.createElement('span');
-            button.type = 'button';
-            button.value = "Check Answer";
-            button.onclick = function () {
-                return _this.checkAnswer();
-            };
-            paragraph.appendChild(button);
-            paragraph.appendChild(this.msg);
-
-            element.append(paragraph);
-        };
-
-        AdditionProblem.prototype.checkAnswer = function () {
-            if (parseInt(this.txt.value) == this.correctAnswer)
-                this.msg.innerHTML = "You are correct";
-            else
-                this.msg.innerHTML = "Please try again";
-        };
         return AdditionProblem;
     })(MathPractice.MathProblem);
     MathPractice.AdditionProblem = AdditionProblem;
--- MathPractice/MathPractice/MathProblem.ts
+++ MathPractice/MathPractice/MathProblem.ts
@@ -2,5 +2,40 @@
 
 module MathPractice {
     export class MathProblem {
+        correctAnswer: number;
+        txt: HTMLInputElement;
+        msg: HTMLSpanElement;
+
+        buildHtml(element: JQuery, tmplate: string) {
+            var renderedProblem = $.tmpl(tmplate, this);
+            var paragraph = document.createElement('p');
+
+            renderedProblem.appendTo(paragraph);
+
+            // not using JQuery here because the rendered problem 
+            // is not in the DOM yet.
+            this.txt = document.createElement('input');
+            this.txt.type = 'text';
+            paragraph.appendChild(this.txt);
+
+            var button = document.createElement('input');
+            this.msg = document.createElement('span');
+            button.type = 'button';
+            button.value = "Check Answer";
+            button.onclick = () => this.checkAnswer();
+            paragraph.appendChild(button);
+            paragraph.appendChild(this.msg);
+
+            element.append(paragraph);
+        }
+
+        checkAnswer() {
+            if (parseInt(this.txt.value) == this.correctAnswer)
+                this.msg.innerHTML = "You are correct";
+            else
+                this.msg.innerHTML = "Please try again";
+        }
+
+
     }
 }
