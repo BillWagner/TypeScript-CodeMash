e7fd87e3e47ae40bf4386546c9ddfa80dcd3e258
Bill Wagner <bill.w.wagner@outlook.com>
2014-01-09 21:21:13 -0500

    Use the subtraction problems too.
    This now shows working implementation inheritance

--- MathPractice/MathPractice/app.js
+++ MathPractice/MathPractice/app.js
@@ -1,8 +1,9 @@
 /// <reference path="scripts/typings/jquery/jquery.d.ts" />
 $(document).ready(function () {
+    var index = 0;
     var el = $('#content');
     var button = $("<input></input>").val("Create New Problem").attr("type", "button").bind("click", function () {
-        var problem = new MathPractice.AdditionProblem(el);
+        var problem = (index++ % 2 === 1) ? new MathPractice.AdditionProblem(el) : new MathPractice.SubtractionProblem(el);
     });
     el.append(button);
 });
--- MathPractice/MathPractice/app.ts
+++ MathPractice/MathPractice/app.ts
@@ -1,12 +1,13 @@
 /// <reference path="scripts/typings/jquery/jquery.d.ts" />
 
 $(document).ready(() => {
+    var index = 0;
     var el = $('#content');
     var button = $("<input></input>")
         .val("Create New Problem")
         .attr("type", "button")
         .bind("click", function () {
-            var problem = new MathPractice.AdditionProblem(el);
+            var problem = (index++ % 2 === 1) ? new MathPractice.AdditionProblem(el) : new MathPractice.SubtractionProblem(el);
         });
     el.append(button);
 });
