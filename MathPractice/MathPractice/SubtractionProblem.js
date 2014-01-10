b1aaf70f2ee14358373a519e4560d6146d083307
Bill Wagner <bill.w.wagner@outlook.com>
2014-01-09 21:18:42 -0500

    Create the subtraction problem constructor
    Now, the class is complete

--- MathPractice/MathPractice/SubtractionProblem.js
+++ MathPractice/MathPractice/SubtractionProblem.js
@@ -1,10 +1,26 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
 var MathPractice;
 (function (MathPractice) {
-    var SubtractionProblem = (function () {
-        function SubtractionProblem() {
+    var SubtractionProblem = (function (_super) {
+        __extends(SubtractionProblem, _super);
+        function SubtractionProblem(element) {
+            _super.call(this);
+
+            this.left = Math.floor(Math.random() * 25);
+            this.right = Math.floor(Math.random() * 25);
+            var tmplate = "${left} - ${right} = ";
+
+            this.correctAnswer = this.left - this.right;
+
+            _super.prototype.buildHtml.call(this, element, tmplate);
         }
         return SubtractionProblem;
-    })();
+    })(MathPractice.MathProblem);
     MathPractice.SubtractionProblem = SubtractionProblem;
 })(MathPractice || (MathPractice = {}));
 //# sourceMappingURL=SubtractionProblem.js.map
--- MathPractice/MathPractice/SubtractionProblem.ts
+++ MathPractice/MathPractice/SubtractionProblem.ts
@@ -1,4 +1,17 @@
 module MathPractice {
-    export class SubtractionProblem {
+    export class SubtractionProblem extends MathProblem {
+        constructor(element: JQuery) {
+            super();
+
+            this.left = Math.floor(Math.random() * 25);
+            this.right = Math.floor(Math.random() * 25);
+            var tmplate = "${left} - ${right} = ";
+
+
+            this.correctAnswer = this.left - this.right;
+
+            super.buildHtml(element, tmplate);
+
+        }
     }
 } 
