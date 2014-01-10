b2136e7d040c45bbbf62042a8be31c3c71acb6c8
Bill Wagner <bill.w.wagner@outlook.com>
2014-01-09 20:26:50 -0500

    Introduce base class
    Because there are similarities on math problems.

---
+++ MathPractice/MathPractice/MathProblem.js
@@ -0,0 +1,10 @@
+var MathPractice;
+(function (MathPractice) {
+    var MathProblem = (function () {
+        function MathProblem() {
+        }
+        return MathProblem;
+    })();
+    MathPractice.MathProblem = MathProblem;
+})(MathPractice || (MathPractice = {}));
+//# sourceMappingURL=MathProblem.js.map
